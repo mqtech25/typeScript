51ec27cb6bd68201f3d1e77fa68769f80d3d88dd
mqtech25 <mqtech25@gmail.com>
2023-02-15 16:53:33 +0500

    modify

--- assignmentPiaic.ts
+++ assignmentPiaic.ts
@@ -145,6 +145,14 @@ console.log(is_sheCome != "yes" ? " True" : "False");
 console.log(is_achivedGoodJob != "yes" ? " True" : "False");
 
 // Q24:More Conditional Tests
+let isPakistanDefualt = "No";
+let isSecure = "Data";
+let isCheck = true;
+console.log(isPakistanDefualt == "No" ? true : false);
+console.log(isPakistanDefualt != "No" ? true : false);
+console.log(isSecure.toLowerCase());
+console.log(isCheck && isPakistanDefualt == "No" ? true : false);
+
 // Q25:Alien Colors
 let alien_color = "green";
 console.log(alien_color == "green" ? "player just earned 5 points" : "");
@@ -179,62 +187,157 @@ if (age < 2) {
 }
 // Q29:Favorite Fruit:
 let favorite_fruits = ["Banana", "Apple", "Grapes"];
-if(favorite_fruits.includes('Banana')){
+if (favorite_fruits.includes("Banana")) {
   console.log("I love Banana");
-}else if(favorite_fruits.includes('orange')){
+} else if (favorite_fruits.includes("orange")) {
   console.log("I love Orange");
-}else if(favorite_fruits.includes('Apple')){
+} else if (favorite_fruits.includes("Apple")) {
   console.log("I love Apple");
-}else if(favorite_fruits.includes('Grapes')){
+} else if (favorite_fruits.includes("Grapes")) {
   console.log("I love Grapes");
-}else{
+} else {
   console.log("I love Every Furit");
 }
 // Q30:Hello Admin:
-let userList=["mqtech","yasir","shahid","bilal","hash"];
+let userList = ["mqtech", "yasir", "shahid", "bilal", "hash"];
 for (let i = 0; i < userList.length; i++) {
   const element = userList[i];
-  if(element=="mqtech"){
+  if (element == "mqtech") {
     console.log(`Hello ${element}, would you like to see a status report?`);
-    
-  }else{
+  } else {
     console.log(`Hello ${element}, thank you for logging in again`);
   }
 }
 // Q31:No Users
-userList=[]
-if(userList.length==0){
-console.log("We need to find some users!");
+userList = [];
+if (userList.length == 0) {
+  console.log("We need to find some users!");
 }
 // Q32:Checking Usernames already
-let current_users=["mqtech","yasir","shahid","bilal","hash"];
-let new_users=['mqtech','hussain','ahmer','ali','Yasir'];
+let current_users = ["mqtech", "yasir", "shahid", "bilal", "hash"];
+let new_users = ["mqtech", "hussain", "ahmer", "ali", "Yasir"];
 for (let i = 0; i < new_users.length; i++) {
   const element = new_users[i];
-  if(current_users.includes(element.toLowerCase())){
+  if (current_users.includes(element.toLowerCase())) {
     console.log("The person will need to enter a new username");
-  }else{
+  } else {
     console.log("the username is available.");
   }
 }
 // Q33:Ordinal Numbers
-let numbers=[1,2,3,4,5,6,7,8,9]
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 for (let i = 0; i < numbers.length; i++) {
   const element = numbers[i];
-  if(element==i+1){
+  if (element == i + 1) {
     console.log(`${element}th`);
   }
 }
 // Q34:Pizzas Array
-let pizzas=['Chicken Fajita','Chicken Tikka','Cheesy Pizza'];
+let pizzas = ["Chicken Fajita", "Chicken Tikka", "Cheesy Pizza"];
 for (let i = 0; i < pizzas.length; i++) {
   const element = pizzas[i];
   console.log(`I like ${element}`);
 }
 // Q35:Animals
-let animals=['frogs','newts','salamanders','toads'];
+let animals = ["frogs", "newts", "salamanders", "toads"];
 for (let i = 0; i < animals.length; i++) {
   const element = animals[i];
   console.log(element);
 }
 console.log("Above Animal shape is like same");
+
+// Q36:T-Shirt:Write a function called make_shirt()
+let shirt_size = 15;
+let shirt_text = "Don't underestimate anyone, Not even yourself";
+make_shirt(shirt_size, shirt_text);
+
+function make_shirt(size, text) {
+  console.log(`Shirt size is ${size} and text on shirt is "${text}"`);
+}
+// Q37:Large Shirts: Modify the make_shirt()
+let shirt_s = "Large & Medium";
+let shirt_tex = "I love TypeScript.";
+update_make_shirt(shirt_s, shirt_tex);
+
+function update_make_shirt(size, text) {
+  console.log(`Shirt size is ${size} and text on shirt is "${text}"`);
+}
+
+// Q38:Write a function called describe_city()
+let city = ["Islamabad", "Peshawar", "lahore"];
+let country = "Pakistan";
+for (let i = 0; i < city.length; i++) {
+  describe_city(city[i], country);
+}
+function describe_city(city, country) {
+  console.log(`${city} is the city of ${country}`);
+}
+
+// Q39:function called city_country()
+let result = city_country("Lahore", "Pakistan");
+console.log(`"${result[0]},${result[1]}"`);
+
+function city_country(city, country) {
+  return [city, country];
+}
+// Q40:function make_album()
+// let albums = {
+//   artist1: {
+//     name: "Fiaz-Ahmad-Fiaz",
+//     title: "Subh-e-Azadi",
+//   },
+//   artist2: {
+//     name: "Mir-Taqi-Mir",
+//     title: "Zikr-mir",
+//   },
+// };
+
+// let albumskEY = Object.keys(albums);
+
+// make_album(albums, albumskEY);
+// function make_album(albums, albumskEY) {
+//   for (const albumskEY in albums) {
+//     console.log(albums[albumskEY]);
+//   }
+// }
+//Q41:Make a array of magician’s names.
+let magican = ["David Copperfield", "Doug Henning", "Siegfried and Roy"];
+show_magicians(magican);
+function show_magicians(magican) {
+  for (let i = 0; i < magican.length; i++) {
+    console.log(magican[i]);
+  }
+}
+// Q42: Write a function called make_great()
+make_great(magican);
+function make_great(magican) {
+  for (let i = 0; i < magican.length; i++) {
+    magican[i] = `The Great ${magican[i]}`;
+  }
+}
+console.log(show_magicians(magican));
+// Q43: function make_great() with a copy of the array of magicians’ names.
+// let magicanCopy = ["David Copperfield", "Doug Henning", "Siegfried and Roy"];
+// console.log(make_great(magicanCopy));
+
+// Q44:Write a function that accepts a array of items a person wants on a sandwich
+let sandwich = ["Chicken Sandwich", "Egg Sandwich", "Seafood Sandwich"];
+for (let i = 0; i < sandwich.length; i++) {
+  person_want_sandwich(sandwich[i]);
+}
+function person_want_sandwich(sandwich) {
+  console.log(`${sandwich} is to delish`);
+}
+// Q45:Write a function that stores information about a car in a Object.
+carFun("civic", "Honda", "2016", "red");
+function carFun(name, manuf, model, color) {
+  let obj = {
+    car: {
+      name: name,
+      manuf: manuf,
+      model: model,
+      color: color,
+    },
+  };
+  console.log(obj);
+}
