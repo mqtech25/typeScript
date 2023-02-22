dfea3d6850305207bb00cd400aedb7192457e91f
mqtech25 <mqtech25@gmail.com>
2023-02-22 16:57:15 +0500

    modfiy

--- dom_typecasting/public/app.js
+++ dom_typecasting/public/app.js
@@ -1,9 +1,21 @@
 "use strict";
-// const input=document.getElementById("#ename")!;
-// console.log(input);
-const form = document.querySelector("#formIncome");
-const input = document.querySelector("#ename");
-form.addEventListener("submit", (e) => {
+let inputArray = document.querySelectorAll(".inputFields");
+let submitBtn = document.querySelector(".calcEmpBtn");
+let allInputInObj = { properties: "" };
+// console.log(inputArray);
+// for (const key in inputArray) {
+//   console.log(inputArray[key]);
+// }
+let eventFunction = (e) => {
     e.preventDefault();
-    console.log(input.value);
-});
+    for (const key in inputArray) {
+        if (inputArray.hasOwnProperty.call(inputArray, key)) {
+            const element = inputArray[key];
+            console.log(element.value, element.id);
+            allInputInObj.properties = element.id;
+            console.log(allInputInObj);
+            allInputInObj.[element.id] = element.value;
+        }
+    }
+};
+submitBtn === null || submitBtn === void 0 ? void 0 : submitBtn.addEventListener("click", eventFunction);
--- dom_typecasting/src/app.ts
+++ dom_typecasting/src/app.ts
@@ -1,10 +1,25 @@
-// const input=document.getElementById("#ename")!;
-// console.log(input);
-const form= document.querySelector("#formIncome") as HTMLFormElement;
-const input=document.querySelector("#ename") as HTMLInputElement;
+let inputArray: any = document.querySelectorAll(".inputFields")!;
+let submitBtn = document.querySelector(".calcEmpBtn")!;
+let allInputInObj: {
+  properties: string;
+} = { properties: "" };
+// console.log(inputArray);
+// for (const key in inputArray) {
+//   console.log(inputArray[key]);
+// }
 
-form.addEventListener("submit",(e:Event)=>{
-    e.preventDefault();
-    console.log(input.value);
+let eventFunction = (e: Event) => {
+  e.preventDefault();
+  for (const key in inputArray) {
+    if (inputArray.hasOwnProperty.call(inputArray, key)) {
+      const element = inputArray[key];
+      console.log(element.value, element.id);
+      allInputInObj.properties = element.id;
+      console.log(allInputInObj);
 
-})
+        allInputInObj.[element.id] = element.value;
+    }
+  }
+};
+
+submitBtn?.addEventListener("click", eventFunction);
